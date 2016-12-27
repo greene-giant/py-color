df40b3e3897be033b97acf59a3ba06281bbc87c9
Greene Giant <greene.giant.github@gmail.com>
2016-12-27 15:03:24 -0800

    Added the wombat colors.

---
+++ wombat.py
@@ -0,0 +1,16 @@
+
+white = "#f6f3e8"
+black = "#242424"
+gray = "#99968b"
+
+green = "#cae682"
+orange = "#ff9b59"
+blue = "#8ac6f2"
+purple = "#aa80fe"
+red = "#e5786d"
+violet = "#c76181"
+
+tan = "#fedfbf"
+
+
+
